3051ed13133a0b79b2a7bcee611e3ab330d497c4
JasonOnes <jasonr.jones14@gmail.com>
2017-06-19 21:01:38 -0700

    working on exception handling errors of inputs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request 
+from flask import Flask, request, redirect
 from caesar import rotate_string
 import cgi
+from time import sleep
 
 app = Flask(__name__)
 app.config['DEBUG'] = True 
@@ -10,29 +11,28 @@ form = """
 <html>
     <head>
         <style>
-            form  {
+            form  {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-    <!-- this is where form goes -->
     <form  action="" method="POST">
-        <label>Rotate by
+        <label>Rotate by(enter a whole number)
         <input type="text" name="rot" />
         </label>
         <textarea name="text">
-            
+            {}
             </textarea>
         <input type="Submit" />
     </body>
@@ -41,12 +41,26 @@ form = """
 
 @app.route('/')
 def index():
-    return form 
+    return form.format("") 
 
 @app.route('/', methods=['POST'])
 def encrypt():
-    rot = int(request.form['rot'])
-    text = cgi.escape(request.form['text'])
-    return "<h1>{0}</h1>".format(rotate_string(text, rot))
+    try:
+        rot = int(request.form['rot'])
+        text = cgi.escape(request.form['text'])
+        if rot and text:
+            return form.format(rotate_string(text, rot))
+    except ValueError:
+        return redirect('/error')
+        """ redirect to index with .format modifying tag of input to error message?"""
+
+@app.route('/error', methods=['POST'])
+def error():
+    "<h1>a numeric rot value must be entered</h1>"
+    
+    #sleep(5)
+    #return redirect('/')
+    return form.format(label="Need to put numeric value for rot", tarea="")
+
     
 app.run()
