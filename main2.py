dca6082206c64c1db3ef899dc46fe6e38d278c97
JasonOnes <jasonr.jones14@gmail.com>
2017-06-22 09:20:43 -0700

    refactored main2 to practice using render_template + exception handler

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from caesar import rotate_string
 import cgi
 from time import sleep
@@ -6,64 +6,24 @@ from time import sleep
 app = Flask(__name__)
 app.config['DEBUG'] = True 
 
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form  {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-    <form  action="" method="POST">
-        <label>{}
-        <input type="number" name="rot" />
-        </label>
-        <textarea name="text">
-            {}
-            </textarea>
-        <input type="Submit" />
-    </body>
-</html>
-"""
 
 @app.route('/')
 def index():
-    return form.format("Rotate by:", "") 
+    return render_template('rot_form.html', declarative="Rotate by")
 
 @app.route('/', methods=['POST'])
 def encrypt():
     try:
         rot = int(request.form['rot'])
-        text = cgi.escape(request.form['text'])
+        text = request.form['text']
         if rot and text:
-            return form.format("Rotated By", rotate_string(text, rot))
+            return render_template('rot_form.html', message=rotate_string(text, rot))
     except ValueError:
-        # if not text:
-        #     return redirect('/')
-        # elif not rot:
-        return redirect('/error')
-        """ redirect to index with .format modifying tag of input to error message?"""
+        return render_template('rot_form.html', declarative="Enter a value", 
+                               message=request.form['text'])
 
-@app.route('/error')
+@app.route('/error', methods=['POST'])
 def error():
-    return form.format("Enter a value!", "")
-
-@app.route('/', methods=['POST'])
-def reroute():
-    return redirect('/')
-
+    return render_template('rot_form.html', declarative="Enter a value!")
     
 app.run()
